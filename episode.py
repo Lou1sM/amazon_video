47fb07c59b78945ef23749a641ed9bfe9a4e6313
Lou1sM <oneillml@tcd.ie>
2023-08-17 18:14:24 +0100

    make run on ss3d, prepare to compute all our rouges and all gpts

--- episode.py
+++ episode.py
@@ -1,6 +1,35 @@
 
 
-class Episode():
+class Episode(): # Nelly stored transcripts and summaries as separate jsons
+    def __init__(self,transcript_data,summary_data):
+        self.transcript = transcript_data['Transcript']
+        #recap_summ = transcript_data['Recap'][0]
+        #trans_summ = transcript_data['Episode Summary'][0]
+        #self.summaries = dict(summary_data,recap=recap_summ,trans_summ=trans_summ)
+        self.summaries = summary_data
+        self.summaries = {k:v for k,v in self.summaries.items() if len(v) > 0}
+        self.title = transcript_data['Show Title'].lower().replace(' ','_')
+        self.show_name = self.title.split('-')[0]
+        self.transcript_data_dict = transcript_data
+        self.summary_data_dict = summary_data
+
+        self.scenes = '\n'.join(self.transcript).split('[SCENE_BREAK]')
+
+    def print_recap(self):
+        for summ in self.summaries:
+            for line in summ.split(' . '):
+                print(line)
+            print()
+
+    def __repr__(self):
+        return f'Episode object for {self.title}'
+
+    def print_transcript(self):
+        for line in self.transcript:
+            print(line)
+
+
+class SSEpisode(): # orig SummScreen stored transcripts and summaries as a single json
     def __init__(self,data_dict):
         self.transcript = data_dict['Transcript']
         self.summaries = data_dict['Recap']
@@ -8,7 +37,7 @@ class Episode():
         self.show_name = self.title.split('-')[0]
         self.data_dict = data_dict
 
-        self.scenes = ''.join(self.transcript).split('[SCENE_BREAK]')
+        self.scenes = '\n'.join(self.transcript).split('[SCENE_BREAK]')
 
     def print_recap(self):
         for summ in self.summaries:
--- summarize_dialogue.py
+++ summarize_dialogue.py
@@ -1,44 +1,106 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+import pandas as pd
+import os
+from os.path import join
 import json
 from episode import Episode
-from torchmetrics.text.rouge import ROUGEScore
+#from torchmetrics.text.rouge import ROUGEScore
+from rouge_score import rouge_scorer
 
 
-#tokenizer = AutoTokenizer.from_pretrained("kabita-choudhary/finetuned-bart-for-conversation-summary")
-#model = AutoModelForSeq2SeqLM.from_pretrained("kabita-choudhary/finetuned-bart-for-conversation-summary")
-
-
-dpipe = pipeline("summarization", model="kabita-choudhary/finetuned-bart-for-conversation-summary")
-pipe = pipeline("summarization", model="facebook/bart-large-cnn")
+def split_text_by_lines(text):
+    lines = text.split('\n')
+    N = len(text.split())
+    first_chunk_size = 0
+    for i,l in enumerate(lines):
+        first_chunk_size += len(l.split())
+        if first_chunk_size > N/2:
+            break
+    first_chunk = '\n'.join(lines[:i+1])
+    second_chunk = '\n'.join(lines[i+1:])
+    return first_chunk, second_chunk
 
 def summarize_scene(text):
+    text = text.replace('@@ ','').replace(' ,',',')
     max_len = min(len(text.split()),50)
     min_len = max(10,max_len-20)
-    return dpipe(text,min_length=min_len, max_length=max_len)[0]['summary_text']
+    print(len(text.split()))
+    if len(text.split())>800:
+        first_chunk, second_chunk = split_text_by_lines(text)
+        return summarize_scene(first_chunk) + summarize_scene(second_chunk)
+    try:
+        return dpipe(text,min_length=min_len, max_length=max_len)[0]['summary_text']
+    except IndexError:
+        first_chunk, second_chunk = split_text_by_lines(text)
+        return summarize_scene(first_chunk) + summarize_scene(second_chunk)
 
 def get_rouges(pred,gt):
-    rouge = ROUGEScore()
-    raw_rscores =rouge(pred,gt)
-    return {'r'+k[5:]: round(v.item(),4) for k,v in raw_rscores.items()}
+    scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL','rougeLsum'], use_stemmer=True)
+    raw_rscores = scorer.score(gt,pred)
+    return {'r'+k[5:]+a: round(getattr(v,a),4) for k,v in raw_rscores.items() for a in ('precision','recall','fmeasure')}
 
-def get_summ_of_summs(concatted_summs,gt):
-    max_len = min(len(gt.split())-50,250)
-    min_len = max(90,max_len-40)
+def get_summ_of_summs(concatted_summs,gt_len):
+    assert type(gt_len) in (int,float)
+    if len(concatted_summs.split())>800:
+        first_chunk, second_chunk = split_text_by_lines(concatted_summs)
+        return get_summ_of_summs(first_chunk,gt_len/2) + get_summ_of_summs(second_chunk,gt_len/2)
+    max_len = int(min(gt_len-50,250))
+    min_len = int(max(90,max_len-40))
     summ_of_summs = pipe(concatted_summs,min_length=min_len, max_length=max_len)[0]['summary_text']
     return summ_of_summs
 
+def harmonic_avg(args):
+    return len(args)/sum([1/x for x in args])
 
 if __name__ == '__main__':
-    with open('SummScreen/tms_train.json') as f:
-        data = json.load(f)
+    import openai
+    openai.api_key = "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"
 
-    for ep_dict in data[:5]:
-        ep = Episode(ep_dict)
+    dpipe = pipeline("summarization", model="kabita-choudhary/finetuned-bart-for-conversation-summary")
+    pipe = pipeline("summarization", model="facebook/bart-large-cnn")
+
+    all_our_bests = {}
+    all_gpt_bests = {}
+    for ep_fname in os.listdir('SummScreen/transcripts'):
+
+        with open(join('SummScreen/transcripts',ep_fname)) as f:
+            transcript_data = json.load(f)
+        if not '[SCENE_BREAK]' in transcript_data['Transcript']: continue
+        with open(join('SummScreen/summaries',ep_fname)) as f:
+            summary_data = json.load(f)
+
+        ep = Episode(transcript_data,summary_data)
 
         concatted_scene_summs = '\n'.join([summarize_scene(x) for x in ep.scenes])
         print('Concatted scene summaries:')
-        print(get_rouges(concatted_scene_summs,ep.summaries[0]))
+        print(len(concatted_scene_summs.split()))
+        gt_len = sum([len(x.split()) for x in ep.summaries.values()])/len(ep.summaries)
+        summ_of_summs = get_summ_of_summs(concatted_scene_summs,gt_len)
+        gpt_summ = openai.ChatCompletion.create(model="gpt-3.5-turbo-16k", messages=[{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": f"Please summarize the following TV show {ep.transcript}"},])['choices'][0]['message']['content']
+        our_best = 0
+        gpt_best = 0
+        for summ_name, gt_summ in ep.summaries.items():
+            print('\n'+summ_name)
+            print('Summary of summaries:')
+            our_scores = get_rouges(summ_of_summs,gt_summ)
+            print(our_scores)
+            our_avg = harmonic_avg([v for k,v in our_scores.items() if 'fmeasure' in k])
+            if our_scores['r2fmeasure'] > our_best:
+                our_best_scores = our_scores
+                our_best = our_scores['r2fmeasure']
+            print('GPT:')
+            gpt_scores = get_rouges(gpt_summ,gt_summ)
+            gpt_avg = harmonic_avg([v for k,v in gpt_scores.items() if 'fmeasure' in k])
+            if gpt_scores['r2fmeasure'] > gpt_best:
+                gpt_best_scores = gpt_scores
+                gpt_best = gpt_scores['r2fmeasure']
+            print(gpt_scores)
 
-        summ_of_summs = get_summ_of_summs(concatted_scene_summs,ep.summaries[0])
-        print('Summary of summaries:')
-        print(get_rouges(summ_of_summs,ep.summaries[0]))
+        print(f'\nBest ours: {our_best_scores}\nBest GPT: {gpt_best_scores}')
+        all_our_bests[ep.title] = our_best_scores
+        all_gpt_bests[ep.title] = gpt_best_scores
+        if len(all_gpt_bests) == 2: break
+    our_df = pd.DataFrame(all_our_bests).T
+    gpt_df = pd.DataFrame(all_gpt_bests).T
+    our_df.to_csv('our_rouge_scores.csv')
+    gpt_df.to_csv('gpt_rouge_scores.csv')
