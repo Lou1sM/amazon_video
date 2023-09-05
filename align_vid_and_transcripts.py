a6fad7445fb630e53ff787b14ca24f0dab8898e7
oneillml <oneillml@tcd.ie>
2023-09-05 10:12:23 +0000

    finish preparing alignment to run on all videos; make scene-captioning run on all videos; tweak the insertion of names in scene captions

--- align_vid_and_transcripts.py
+++ align_vid_and_transcripts.py
@@ -1,4 +1,6 @@
 import numpy as np
+from context_lib import redirect_stdout
+import os
 from dl_utils.misc import check_dir
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import json
@@ -16,10 +18,8 @@ def clean(line):
 def cc_clean(line):
     return line.replace('[ __ ] ','').strip()
 
-def dist_func(a,b):
-    return 1 if a=='' or b=='' else 1-SequenceMatcher(a=a,b=b).ratio()
-
 def align(xlines,ylines):
+    dist_mat_ = []
     sm = SequenceMatcher()
     for xl in xlines:
         if len(xl)==0:
@@ -41,84 +41,93 @@ def align(xlines,ylines):
     alignment = dtw(dist_mat)
     return alignment
 
+def split_by_alignment(ep_name):
+    with open(f'SummScreen/transcripts/{ep_name}.json') as f:
+        transcript_data = json.load(f)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-t','--is_test',action='store_true')
-parser.add_argument('--db_failed_scenes',action='store_true')
-parser.add_argument('--print_full_aligned',action='store_true')
-parser.add_argument('--print_tlines',action='store_true')
-parser.add_argument('--ep_name',type=str, default='oltl-10-18-10')
-ARGS = parser.parse_args()
+    with open(f'SummScreen/closed_captions/{ep_name}.json') as f:
+        closed_captions = json.load(f)
+
+    if not '[SCENE_BREAK]' in transcript_data['Transcript']:
+        print(f'There doesn\'t appear to be scene break markings for {ep_name}')
+        return
+
+    raw_transcript_lines = transcript_data['Transcript']
+    transcript_lines = [word_tokenize(clean(line)) for line in raw_transcript_lines]
+    cc_lines = [word_tokenize(cc_clean(x[1])) for x in closed_captions['captions']]
+    cc_timestamps = [x[0] for x in closed_captions['captions']]
+    if ARGS.is_test:
+        transcript_lines = transcript_lines[:40]
+        cc_lines = cc_lines[:40]
+        cc_timestamps = cc_timestamps[:40]
+    all_words, counts = np.unique(sum(cc_lines+transcript_lines,[]),return_counts=True)
+
+    video_fpath = f'SummScreen/videos/{ep_name}.mp4'
+    alignment = align(transcript_lines, cc_lines)
+
+    if ARGS.print_full_aligned:
+        for i,j in zip(alignment.index1,alignment.index2):
+            print(transcript_lines[i],cc_lines[j], cc_timestamps[j])
+
+    def secs_from_timestamp(timestamp):
+        hrs,mins,secs_ = timestamp.split(':')
+        secs, msecs = secs_.split(',')
+        return 3600*float(hrs) + 60*float(mins) + float(secs) + 1e-3*float(msecs)
+
+    timestamped_lines = []
+    starttime = 0
+    endtime = 0
+    cur_idx = 0
+    check_dir(f'SummScreen/video_scenes/{ep_name}')
+    scene_num = 0
+    scene_starttime = 0
+    scene_endtime = 0
+    for idx1, idx2 in zip(alignment.index1,alignment.index2):
+        new_starttime, new_endtime = cc_timestamps[idx2].split(' --> ')
+        new_starttime = secs_from_timestamp(new_starttime)
+        new_endtime = secs_from_timestamp(new_endtime)
+        if idx1!=cur_idx or (idx1==alignment.index1.max() and idx2==alignment.index2.max()): # increment transcript lines
+            timestamped_tline = f'{starttime} --> {endtime} {raw_transcript_lines[cur_idx]}'
+            timestamped_lines.append(timestamped_tline)
+            if ARGS.print_tlines:
+                print(timestamped_tline)
+            if raw_transcript_lines[cur_idx] == '[SCENE_BREAK]' or (idx1==alignment.index1.max() and idx2==alignment.index2.max()): # increment scenes too
+                outpath = f'SummScreen/video_scenes/{ep_name}/{ep_name}_scene{scene_num}.mp4'
+                scene_endtime = min(new_starttime,endtime)
+                scene_endtime -= 3 + (scene_endtime - scene_starttime)/8 #cut further reduce overspill
+                print(f'SCENE{scene_num}: {scene_starttime}-{scene_endtime}')
+                if scene_starttime >= scene_endtime and ARGS.db_failed_scenes:
+                    breakpoint()
+                with redirect_stdout(None):
+                    ffmpeg_extract_subclip(video_fpath,scene_starttime, scene_endtime, targetname=outpath)
+                scene_num += 1
+                scene_starttime = max(new_starttime,endtime) # start of next scene should be greater than both start of first caption in the next scene and end of last caption in this scene
+            assert len(timestamped_lines) == cur_idx+1
+            cur_idx = idx1
+            starttime = new_starttime
+            endtime = new_endtime
+        if new_starttime < starttime:
+            starttime = new_starttime
+            print(777)
+        if new_endtime < endtime:
+            print(888)
+        if new_endtime > endtime:
+            endtime = new_endtime
 
 
-with open(f'SummScreen/transcripts/{ARGS.ep_name}.json') as f:
-    transcript_data = json.load(f)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t','--is_test',action='store_true')
+    parser.add_argument('--db_failed_scenes',action='store_true')
+    parser.add_argument('--print_full_aligned',action='store_true')
+    parser.add_argument('--print_tlines',action='store_true')
+    parser.add_argument('--ep_name',type=str, default='oltl-10-18-10')
+    ARGS = parser.parse_args()
 
-with open(f'SummScreen/closed_captions/{ARGS.ep_name}.json') as f:
-    closed_captions = json.load(f)
-
-raw_transcript_lines = transcript_data['Transcript']
-transcript_lines = [word_tokenize(clean(line)) for line in raw_transcript_lines]
-cc_lines = [word_tokenize(cc_clean(x[1])) for x in closed_captions['captions']]
-cc_timestamps = [x[0] for x in closed_captions['captions']]
-if ARGS.is_test:
-    transcript_lines = transcript_lines[:40]
-    cc_lines = cc_lines[:40]
-    cc_timestamps = cc_timestamps[:40]
-all_words, counts = np.unique(sum(cc_lines+transcript_lines,[]),return_counts=True)
-word_to_count = dict(zip(all_words,counts))
-N = len(all_words)
-
-video_fpath = f'SummScreen/videos/{ARGS.ep_name}.mp4'
-dist_mat = np.array([[dist_func(a,b) for a in cc_lines] for b in transcript_lines])
-dist_mat_ = []
-alignment = align(transcript_lines, cc_lines)
-
-if ARGS.print_full_aligned:
-    for i,j in zip(alignment.index1,alignment.index2):
-        print(transcript_lines[i],cc_lines[j], cc_timestamps[j])
-
-def secs_from_timestamp(timestamp):
-    hrs,mins,secs_ = timestamp.split(':')
-    secs, msecs = secs_.split(',')
-    return 3600*float(hrs) + 60*float(mins) + float(secs) + 1e-3*float(msecs)
-
-timestamped_lines = []
-starttime = 0
-endtime = 0
-cur_idx = 0
-check_dir(f'SummScreen/video_scenes/{ARGS.ep_name}')
-scene_num = 0
-scene_starttime = 0
-scene_endtime = 0
-for idx1, idx2 in zip(alignment.index1,alignment.index2):
-    new_starttime, new_endtime = cc_timestamps[idx2].split(' --> ')
-    new_starttime = secs_from_timestamp(new_starttime)
-    new_endtime = secs_from_timestamp(new_endtime)
-    if idx1!=cur_idx or (idx1==alignment.index1.max() and idx2==alignment.index2.max()): # increment transcript lines
-        timestamped_tline = f'{starttime} --> {endtime} {raw_transcript_lines[cur_idx]}'
-        timestamped_lines.append(timestamped_tline)
-        if ARGS.print_tlines:
-            print(timestamped_tline)
-        if raw_transcript_lines[cur_idx] == '[SCENE_BREAK]' or (idx1==alignment.index1.max() and idx2==alignment.index2.max()): # increment scenes too
-            outpath = f'SummScreen/video_scenes/{ARGS.ep_name}/{ARGS.ep_name}_scene{scene_num}.mp4'
-            scene_endtime = min(new_starttime,endtime)
-            scene_endtime -= 3 + (scene_endtime - scene_starttime)/8 # cut further reduce overspill
-            print(f'SCENE{scene_num}: {scene_starttime}-{scene_endtime}')
-            if scene_starttime >= scene_endtime and ARGS.db_failed_scenes:
-                breakpoint()
-            ffmpeg_extract_subclip(video_fpath,scene_starttime, scene_endtime, targetname=outpath)
-            scene_num += 1
-            scene_starttime = max(new_starttime,endtime) # start of next scene should be greater than both start of first caption in the next scene and end of last caption in this scene
-        assert len(timestamped_lines) == cur_idx+1
-        cur_idx = idx1
-        starttime = new_starttime
-        endtime = new_endtime
-    if new_starttime < starttime:
-        starttime = new_starttime
-        print(777)
-    if new_endtime < endtime:
-        print(888)
-    if new_endtime > endtime:
-        endtime = new_endtime
+    if ARGS.ep_name == 'all':
+        all_ep_names = [fn[:-4] for fn in os.listdir('SummScreen/videos') if fn.endswith('.mp4')]
+        for en in all_ep_names:
+            split_by_alignment(en)
+    else:
+        split_by_alignment(ARGS.ep_name)
 
--- caption_each_scene.py
+++ caption_each_scene.py
@@ -27,8 +27,7 @@ def maybe_replace_subj(char_name, indeternp, cap):
     else:
         return cap
 
-def caption_each_scene(ep_name, vl_transformer, tokenizer, tensorizer):
-    img_res = 224
+def caption_each_scene(ep_name, vl_transformer, tokenizer, tensorizer, img_res):
     max_num_frames = 64
 
     scenes_dir = f'SummScreen/video_scenes/{ep_name}'
@@ -37,33 +36,55 @@ def caption_each_scene(ep_name, vl_transformer, tokenizer, tensorizer):
     #scene_fnames = sorted(os.listdir(scenes_dir),key=lambda x: int(x.split('_')[1].split('.')[0][5:]))
     scene_fnames = [x for x in os.listdir(scenes_dir) if x.endswith('mp4')]
     scene_nums = sorted([int(x.split('_')[1][5:-4]) for x in scene_fnames])
+    if len(ep.scenes) == 1: # prob means no scene break markings
+        print(f'There doesn\'t appear to be scene break markings for {ep_name}')
+        return
     for sn in scene_nums:
         #scene_vid_path = os.path.join(scenes_dir,sfn)
+        #if sn == 48:
         scene_vid_path = os.path.join(scenes_dir,f'{ep_name}_scene{sn}.mp4')
         frames, _ = extract_frames_from_video_path(
                     scene_vid_path, target_fps=3, num_frames=64,
                     multi_thread_decode=False, sampling_strategy="uniform",
                     safeguard_duration=False, start=None, end=None)
-        cap = 'jim'
+        cap = inference(scene_vid_path, img_res, max_num_frames, vl_transformer, tokenizer, tensorizer)
+        #else:
+            #cap = 'jim'
         scene_transcript = ep.scenes[sn]
-        if sn==28:
-            cap = inference(scene_vid_path, img_res, max_num_frames, vl_transformer, tokenizer, tensorizer)
-            breakpoint()
         appearing_chars = set([x.split(':')[0] for x in scene_transcript.split('\n') if not x.startswith('[') and len(x) > 0])
 
         orig_cap = cap
-        for cn in appearing_chars:
-            g = gender(cn)
-            if g=='m':
-                cap = maybe_replace_subj(cn,'a man',cap)
-                cap = maybe_replace_subj(cn,'a boy',cap)
-            elif g=='f':
-                cap = maybe_replace_subj(cn,'a woman',cap)
-                cap = maybe_replace_subj(cn,'a girl',cap)
-            elif len(appearing_chars)==1:
-                cap = maybe_replace_subj(cn,'a person',cap)
-                cap = maybe_replace_subj(cn,'someone',cap)
-                cap = maybe_replace_subj(cn,'somebody',cap)
+        appearing_maybe_males = [c for c in appearing_chars if gender(c) in ['m','a']]
+        appearing_maybe_females = [c for c in appearing_chars if gender(c) in ['f','a']]
+        if len(appearing_maybe_males)==1:
+            if appearing_maybe_males[0] in appearing_maybe_females:
+                appearing_maybe_females.remove(appearing_maybe_males[0])
+        if len(appearing_maybe_females)==1:
+            if appearing_maybe_females[0] in appearing_maybe_males:
+                appearing_maybe_males.remove(appearing_maybe_females[0])
+
+        if len(appearing_maybe_males)==1:
+            if 'a man' in cap:
+                cap = cap.replace('a man',appearing_maybe_males[0], 1)
+            elif 'a boy' in cap:
+                cap = cap.replace('a boy',appearing_maybe_males[0], 1)
+        if len(appearing_maybe_females)==1:
+            if 'a woman' in cap:
+                cap = cap.replace('a woman',appearing_maybe_females[0], 1)
+            elif 'a girl' in cap:
+                cap = cap.replace('a girl',appearing_maybe_females[0], 1)
+        #for cn in appearing_chars:
+        #    g = gender(cn)
+        #    if g=='m':
+        #        cap = maybe_replace_subj(cn,'a man',cap)
+        #        cap = maybe_replace_subj(cn,'a boy',cap)
+        #    elif g=='f':
+        #        cap = maybe_replace_subj(cn,'a woman',cap)
+        #        cap = maybe_replace_subj(cn,'a girl',cap)
+        #    elif len(appearing_chars)==1:
+        #        cap = maybe_replace_subj(cn,'a person',cap)
+        #        cap = maybe_replace_subj(cn,'someone',cap)
+        #        cap = maybe_replace_subj(cn,'somebody',cap)
         print(f'SCENE{sn}: {cap}\t{orig_cap}')
         caps_per_scene.append(cap)
 
@@ -79,6 +100,8 @@ if __name__ == '__main__':
     parser.add_argument('--ep_name',type=str, default='oltl-10-18-10')
     ARGS = parser.parse_args()
 
+    img_res = 224
+
     bert_model, config, tokenizer_ = get_bert_model(do_lower_case=True)
     swin_model = get_swin_model(img_res, 'base', '600', False, True)
     vl_transformer_ = VideoTransformer(True, config, swin_model, bert_model)
@@ -88,13 +111,12 @@ if __name__ == '__main__':
     vl_transformer_.cuda()
     vl_transformer_.eval()
 
-    tensorizer_ = build_tensorizer(tokenizer_, 50, 1568, 50, is_train=False)
+    tensorizer_ = build_tensorizer(tokenizer_, 150, 1568, 50, is_train=False)
 
     if ARGS.ep_name == 'all':
-        all_ep_names = [fn[:-4] for fn in os.listdir('SummScreen/videos') if fn.endswith('.mp4')]
-        for en in all_ep_names:
-            caption_each_scene(fn, vl_transformer_, tokenizer_, tensorizer_)
+        all_ep_names = [fn[:-4] for fn in os.listdir('SummScreen/video_scenes') if fn.endswith('.mp4')]
+        for en in os.listdir('SummScreen/video_scenes'):
+            caption_each_scene(en, vl_transformer_, tokenizer_, tensorizer_, img_res)
     else:
-        caption_each_scene(ARGS.ep_name, vl_transformer_, tokenizer_, tensorizer_)
-
+        caption_each_scene(ARGS.ep_name, vl_transformer_, tokenizer_, tensorizer_, img_res)
 
