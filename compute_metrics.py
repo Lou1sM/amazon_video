2b8d428c1a43b97d2736c7f8811f588175967157
oneillml <oneillml@tcd.ie>
2024-01-21 13:56:19 +0000

    FEAT: implement reverse factscore and bertscore; REFACTOR: make list of cl-arg metrics, better able to handle addition of new metrics in the future

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+from evaluate import load
 import json
 from factscore.factscorer import FactScorer
 from dl_utils.misc import check_dir
@@ -11,16 +13,16 @@ from factscore.atomic_facts import AtomicFactGenerator
 from tqdm import tqdm
 
 
-def get_maybe_cached_atomic_facts(maybe_cached_path, generator, pred=None, path=None):
-    assert (pred is None) != (path is None)
+def get_maybe_cached_atomic_facts(maybe_cached_path, generator, nl_text=None, path=None):
+    assert (nl_text is None) != (path is None)
     if os.path.exists(maybe_cached_path):
         with open(maybe_cached_path) as f:
             pred_facts = f.readlines()
     else:
-        if pred is None:
+        if nl_text is None:
             with open(path) as f:
                 pred = f.read()
-        pred_facts_and_sources, para_breaks = generator.run(pred)
+        pred_facts_and_sources, para_breaks = generator.run(nl_text)
         pred_facts = [x for line in pred_facts_and_sources for x in line[1]]
         with open(maybe_cached_path,'w') as f:
             f.write('\n'.join([pf for pf in pred_facts]))
@@ -28,21 +30,39 @@ def get_maybe_cached_atomic_facts(maybe_cached_path, generator, pred=None, path=
     return pred_facts
 
 
+def get_factscore(facts, knowledge_base_text, epname): #epname need for caching
+    factscore = fs.get_score(epname, topics=['A summary of a TV show'],
+                  ref_summaries=[knowledge_base_text],
+                  atomic_facts=[facts])
+
+    factscore['decisions'] = [{k:int(v) if v in [True,False] else v for k,v in dec.items()} for dec in factscore['decisions'][0]]
+    return factscore
+
 if __name__ == '__main__':
     generator = AtomicFactGenerator("factscore/api.key", "factscore/demos", gpt3_cache_file=None)
 
+    all_metrics = ['factscore', 'rev-factscore', 'rouge', 'bertscore']
     parser = argparse.ArgumentParser()
     parser.add_argument('--expname',type=str,default='tmp')
     parser.add_argument('-t','--is_test',action='store_true')
     parser.add_argument('--overwrite',action='store_true')
     parser.add_argument('--run_factscore',action='store_true')
     parser.add_argument('--only_get_facts',action='store_true')
+    parser.add_argument('--is_reversed',action='store_true')
     parser.add_argument('--llama_size', type=str)
+    parser.add_argument('--metrics', type=str, nargs='+', choices=all_metrics+['all'], default=['all'])
     ARGS = parser.parse_args()
-    assert ARGS.llama_size in ('7B', '13B', '70B')
+    #assert ARGS.llama_size in ('7B', '13B', '70B')
+    if ARGS.metrics == ['all']:
+        ARGS.metrics = all_metrics
+    if 'rouge' in ARGS.metrics:
+        ARGS.metrics = [m for m in ARGS.metrics if m!='rouge']+['r1','r2','rl','rlsum']
+    if 'bertscore' in ARGS.metrics:
+        ARGS.metrics = [m for m in ARGS.metrics if m!='bertscore']+['bs-precision','bs-recall','bs-f1']
 
 
-    fs = FactScorer(model_name=f'retrieval+llama{ARGS.llama_size}+npm',
+    llama_size = '7B' if ARGS.is_test else '13B'
+    fs = FactScorer(model_name=f'retrieval+llama{llama_size}+npm',
                     data_dir=".cache/factscore/",
                     model_dir="huggyllama",
                     cache_dir=".cache/factscore/",
@@ -56,44 +76,71 @@ if __name__ == '__main__':
     expdir = f'experiments/{ARGS.expname}'
     gendir = join(expdir, 'generations_test')
     all_epnames = os.listdir(gendir)
-    full_results = {}
+    full_results = {m:{} for m in ARGS.metrics}
     all_factscores = []
     all_rouges = []
-    for fn in tqdm(all_epnames):
+    for i,fn in enumerate(pbar := tqdm(all_epnames)):
         assert fn.endswith('.txt')
         epname = fn[:-4]
 
+        check_dir(epdir := join(expdir,epname))
         ep = episode_from_epname(epname)
+        gt_summs = ep.summaries # this is a dict
 
-        #check_dir(cache_dir := 'SummScreen/cached_llama_facts/full_pred_summ_facts')
-        check_dir(cache_dir := f'../gpt-3.5-turbo-instruct-facts/{ARGS.expname}')
-        cache_fpath = f'{cache_dir}/{epname}'
-        summ_fpath = f'{gendir}/{epname}.txt'
-        with open(summ_fpath) as f:
-            pred = f.read()
-        atomic_facts = get_maybe_cached_atomic_facts(cache_fpath, generator, pred=pred)
+        with open(f'{gendir}/{epname}.txt') as f:
+            pred_summ = f.read()
 
-        if not ARGS.only_get_facts:
-            factscore = fs.get_score(epname, topics=['A summary of a TV show'],
-                          ref_summaries=[ep.summaries],
-                          atomic_facts=[atomic_facts])
 
-            factscore['decisions'] = [{k:int(v) if v in [True,False] else v for k,v in dec.items()} for dec in factscore['decisions'][0]]
-            rouge_score = rouge_from_multiple_refs(pred, ep.summaries.values(), benchmark_rl=True, return_full=False)
-            full_results[epname] = {'factscore':factscore, 'rouge':rouge_score}
-            all_factscores.append(factscore['score'])
-            all_rouges.append(rouge_score)
-        if ARGS.is_test:
+        # Now compute specified metrics
+        if 'factscore' in ARGS.metrics:
+            pbar.set_description(f'Computing factscore for {epname}')
+            check_dir(cache_dir := f'../gpt-3.5-turbo-instruct-facts/{ARGS.expname}')
+            cache_fpath = f'{cache_dir}/{epname}'
+            pred_facts = get_maybe_cached_atomic_facts(cache_fpath, generator, nl_text=pred_summ)
+            fs_results = get_factscore(pred_facts, gt_summs, epname)
+            full_results['factscore'][epname] = fs_results['score']
+
+        if 'rev-factscore' in ARGS.metrics:
+            pbar.set_description(f'Computing rev-factscore for {epname}')
+            check_dir(cache_dir := '../gpt-3.5-turbo-instruct-facts/gt_summ_facts')
+            facts_by_summ_name = {}
+            gt_facts = []
+            for k,v in gt_summs.items():
+                cache_fpath = f'{cache_dir}/{epname}-{k}'
+                new_facts = get_maybe_cached_atomic_facts(cache_fpath, generator, nl_text=v)
+                gt_facts += new_facts
+            gt_facts = list(set(gt_facts))
+            rfs_results = get_factscore(gt_facts, {'pred':pred_summ}, f'{epname}_{ARGS.expname}')
+            full_results['rev-factscore'][epname] = rfs_results['score']
+
+        if 'r1' in ARGS.metrics:
+            pbar.set_description(f'Computing rouge for {epname}')
+            rouge_results = rouge_from_multiple_refs(pred_summ, gt_summs.values(), benchmark_rl=True, return_full=False)
+            for r,s in zip(['r1','r2','rl','rlsum'], rouge_results):
+                full_results[r][epname] = s
+
+        if 'bs-f1' in ARGS.metrics:
+            pbar.set_description(f'Computing bertscore for {epname}')
+            bertscore = load("bertscore")
+            all_bss = bertscore.compute(predictions=[pred_summ]*len(gt_summs), references=list(gt_summs.values()), lang="en")
+            summ_idx_to_pick = np.array(all_bss['f1']).argmax()
+            for k,v in all_bss.items():
+                if k!='hashcode':
+                    full_results[f'bs-{k}'][epname] = v[summ_idx_to_pick]
+
+        if ARGS.is_test and i==1:
             break
-    if not ARGS.only_get_facts:
-        rouges_arr = np.array(all_rouges).mean(axis=0)
-        mean_factscore = np.array(all_factscores).mean()
 
-        with open(join(expdir, 'rouge_and_factscore_results.txt'), 'w') as f:
-            f.write(f'Expname: {ARGS.expname}\nFActScore: {mean_factscore:.4f}\n')
-            f.write(f'Expname: {ARGS.expname}\n')
-            for r,s in zip(['1','2','L','Lsum'], rouges_arr):
-                f.write(f'rouge{r}: {s}\n')
+    results_df = pd.DataFrame(full_results)
+    results_df.loc['mean'] = results_df.mean(axis=0)
+    results_df.loc['std'] = results_df.std(axis=0)
+    assert set(results_df.columns) == set(ARGS.metrics)
 
-        with open(join(expdir,'full_rouge_and_factscore_results.json'),'w') as f:
-            json.dump(full_results, f)
+    for m in ARGS.metrics:
+        m_results = results_df[m].to_dict()
+        #v['mean'] = sum(v.values())/len(v)
+        #v['std'] = (sum((x-v['mean'])**2 for x in v.values())/len(v))**.5
+        with open(join(expdir, f'{k}-full-results.json'), 'w') as f:
+            json.dump(m_results,f)
+
+    results_df.to_csv(join(expdir, 'full_results.csv'))
