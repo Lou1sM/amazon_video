c5514d61de857d19a21eaed1be3c39252df6c5e2
Lou1sM <lmahonology@gmail.com>
2024-09-01 12:14:23 +0800

    clique to exclude non-char images

--- scrape_actor_faces.py
+++ scrape_actor_faces.py
@@ -1,54 +1,68 @@
 from selenium import webdriver
+import os
+import shutil
+import json
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+import selenium
 import io
 from os.path import join
 import time
+import numpy as np
 from dl_utils.misc import check_dir
 import requests
 from PIL import Image
-from faces_train.facenet_pytorch import MTCNN
-from utils import prepare_for_pil
+#from faces_train.facenet_pytorch import MTCNN
+#from utils import prepare_for_pil
+from deepface import DeepFace
+from nltk.corpus import names
+from utils import path_list
+
+male_names = names.words('male.txt')
+female_names = names.words('female.txt')
 
 
-mtc = MTCNN(image_size=160, margin=10, min_face_size=20, thresholds=[0.8, 0.8, 0.9], factor=0.709, post_process=True, device='cuda', keep_all=True)
-
-def save_and_crop_img_from_url(file_path:str,url:str):
+def save_and_crop_img_from_url(file_path:str,url:str, char_name):
     try:
         image_content = requests.get(url).content
     except Exception as e:
         print(f"ERROR - Could not download {url} - {e}")
     image_file = io.BytesIO(image_content)
-    image = Image.open(image_file).convert('RGB')
-    face_img = mtc(image)
-    if face_img is None: # just skip if other chars in the image
-        print(f"found 0 faces in {url} so skipping")
-        return
-    if len(face_img) > 1: # just skip if other chars in the image
-        print(f"found {len(face_img)} faces in {url} so skipping")
-        return
-    pil_face_img = Image.fromarray(prepare_for_pil(face_img.squeeze(0)))
-    if 'Krendler' in file_path:
-        breakpoint()
-    with open(file_path, 'wb') as f:
-        pil_face_img.save(f, "JPEG", quality=95)
-    print(f"SUCCESS - saved {url} - as {file_path}")
+    image_ar = np.array(Image.open(image_file))
+    detected_faces = DeepFace.extract_faces(image_ar, enforce_detection=False)
+    detected_faces.sort(key=lambda x:x['confidence'], reverse=True)
+
+    if len(detected_faces)==1 and (conf:=detected_faces[0]['confidence']) > 0.5:
+        det_gender = DeepFace.analyze(image_ar, actions='gender')[0]['dominant_gender']
+        if char_name.split(' ')[0] in female_names and det_gender == 'Man':
+            print(f'excluding cuz name is female but face is male')
+            return
+        if char_name.split(' ')[0] in male_names and det_gender == 'Woman':
+            print(f'excluding cuz name is male but face is female')
+            return
+        np.save(file_path, image_ar) # deepface expects whole image for comparison and extraction, not just the face
+        print(f"SUCCESS - saved {url} - as {file_path}")
 
 def scrape_from_url(url, movie_name):
+    check_dir(raw_char_faces_dir:=join('data/raw_scraped_char_faces', movie_name))
     check_dir(char_faces_dir:=join('data/scraped_char_faces', movie_name))
     with webdriver.Chrome() as wd:
         actions = ActionChains(wd)
         wd.get(url)
         char_names = [x.find_element(By.TAG_NAME, 'span').text for x in  wd.find_elements(By.CSS_SELECTOR, 'a[data-testid=cast-item-characters-link]')]
         time.sleep(1)
-        wd.find_element(By.CSS_SELECTOR, 'button[data-testid=reject-button]').click() # cookie consent shite
+        try:
+            wd.find_element(By.CSS_SELECTOR, 'button[data-testid=reject-button]').click() # cookie consent shite
+        except selenium.common.exceptions.NoSuchElementException:
+            pass
         wd.maximize_window()
         time.sleep(1)
         wd.execute_script("window.scrollTo(0, 2000)")
         time.sleep(1)
 
+        nimages_per_char = {}
         for char_name in char_names:
-            check_dir(char_dir:=join(char_faces_dir, char_name))
+            check_dir(raw_char_dir:=join(raw_char_faces_dir, char_name))
             tcd = wd.find_element(By.LINK_TEXT, char_name).find_element(By.XPATH, '..')
             actions.move_to_element(tcd).perform()
             tcd.click()
@@ -57,11 +71,46 @@ def scrape_from_url(url, movie_name):
                 img_children = cfe.find_elements(By.TAG_NAME, 'img')
                 assert len(img_children) == 1
                 img_url = img_children[0].get_attribute('src')
-                save_fpath = join(char_dir, f'img{i}.jpg')
-                save_and_crop_img_from_url(file_path=save_fpath, url=img_url)
+                #save_fpath = join(char_dir, f'img{i}.jpg')
+                save_fpath = join(raw_char_dir, f'img{i}.npy')
+                save_and_crop_img_from_url(file_path=save_fpath, url=img_url, char_name=char_name)
+            nimages_per_char[char_name] = len(char_face_elements)
+            maybe_more_imgs = wd.find_elements(By.CSS_SELECTOR, 'a[class=titlecharacters-image-grid__see-more-link]')
+            if len(maybe_more_imgs) > 0:
+                assert len(maybe_more_imgs) == 1
+                more_imgs_text = maybe_more_imgs[0].text
+                assert more_imgs_text.endswith(' more photos')
+                nimages_per_char[char_name] += int(more_imgs_text.removesuffix(' more photos'))
+
+            print(nimages_per_char[char_name])
+            import networkx as nx
+            saved_fpaths = path_list(raw_char_dir)
+            N = len(saved_fpaths)
+            if N > 0:
+                cross_verified = np.eye(N)
+                for i in range(N):
+                    for j in range(i+1,N):
+                        is_verified = DeepFace.verify(np.load(saved_fpaths[i]), np.load(saved_fpaths[j]))['verified']
+                        cross_verified[i,j] = is_verified
+                        cross_verified[j,i] = is_verified
+
+                G = nx.from_numpy_array(cross_verified)
+                max_clique = max(nx.algorithms.clique.find_cliques(G), key=len)
+                check_dir(char_dir:=join(char_faces_dir, char_name))
+                for i,fp in enumerate(saved_fpaths):
+                    if i in max_clique:
+                        new_fp = os.path.join(char_dir, os.path.basename(fp))
+                        shutil.copy(fp, new_fp)
+
             time.sleep(1)
             wd.back()
             time.sleep(0.5)
+
+        #nimages_per_char = {k:(v+1)/(sum(nimages_per_char.values())+1) for k,v in nimages_per_char.items()}
+        with open(f'data/nimages-per-char/{movie_name}-nimages-per-char.json', 'w') as f:
+            json.dump(nimages_per_char, f)
+
+
 
 
 scrape_from_url('https://www.imdb.com/title/tt0102926/?ref_=fn_al_tt_1', 'silence-of-lambs')
