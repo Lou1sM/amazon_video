44e989b4805e0321e179a2593346fd5b7f96c2d0
Lou1sM <lmahonology@gmail.com>
2025-08-07 12:27:55 +0800

    FEAT: multiple frame encoder model options; BUG: loaded cached frames if uniform

--- simplified_segment.py
+++ simplified_segment.py
@@ -1,4 +1,5 @@
 import os
+from time import time
 import io
 import sys
 import math
@@ -17,7 +18,7 @@ FFMPEG_PATH = imageio_ffmpeg.get_ffmpeg_exe()
 LOG2PI = 1.837877
 
 class SceneSegmenter():
-    def __init__(self, dset_name, show_name, season, max_seg_size, pow_incr, use_avg_sig, kf_every, use_log_dist_cost, device):
+    def __init__(self, dset_name, show_name, season, max_seg_size, pow_incr, use_avg_sig, kf_every, use_log_dist_cost, device, model_name):
         self.dset_name = dset_name
         self.max_seg_size = max_seg_size
         self.pow_incr = pow_incr
@@ -26,6 +27,8 @@ class SceneSegmenter():
         self.fps = 1/kf_every
         self.use_log_dist_cost = use_log_dist_cost
         self.device = device
+        self.model_name = model_name
+        self.feats_device = 'cuda'
         assert (show_name is None) == (season is None)
         if show_name is None:
             self.name_path = dset_name
@@ -33,8 +36,8 @@ class SceneSegmenter():
             self.name_path = join(dset_name, show_name, f'season_{season}')
         self.vid_dir = join('data/full-videos', self.name_path)
         self.base_framesdir = join('data/ffmpeg-keyframes', self.name_path)
-        self.base_featsdir =  join('data/ffmpeg-frame-features', self.name_path)
-        self.cached_splits_dir = join('data/cached-splits', self.name_path)
+        self.base_featsdir =  join('data/ffmpeg-frame-features', self.name_path, self.model_name)
+        self.cached_splits_dir = join('data/cached-splits', self.name_path, self.model_name)
         os.makedirs(self.base_framesdir, exist_ok=True)
         os.makedirs(self.base_featsdir, exist_ok=True)
         os.makedirs(self.cached_splits_dir, exist_ok=True)
@@ -45,21 +48,21 @@ class SceneSegmenter():
         vid_fpath = f"{self.vid_dir}/{vidname}.mp4"
         assert os.path.exists(vid_fpath), f'{vid_fpath} does not exist'
         duration = float(ffmpeg.probe(vid_fpath)["format"]["duration"])
-        if uniform:
+        starttime = time()
+        runtime_fp=f'ss-runtimes/frames/{vidname}'
+        if os.path.isfile(frames_fp:=join(framesdir, 'frametimes.npy')) and os.path.isfile(runtime_fp) and (not recompute):
+            timepoints = np.load(join(framesdir, 'frametimes.npy'))
+        elif uniform:
             for fn in os.listdir(framesdir):
                 os.remove(join(framesdir, fn))
             intdur = int(duration - 1/self.fps)
             end_timestamp = f"{intdur//3600:02}:{(intdur%3600)//60:02}:{intdur%60:02}"
             subprocess.run([FFMPEG_PATH, '-hide_banner', '-loglevel', 'error', '-ss', f'00:00:{int(1/self.fps):02}', '-to', end_timestamp, '-i', vid_fpath, '-vf', f'fps={self.fps}', f"{framesdir}/%05d.jpg"])
-            #subprocess.run([FFMPEG_PATH, '-ss', f'00:00:{int(1/self.fps):02}', '-to', end_timestamp, '-i', vid_fpath, '-vf', f'fps={self.fps}', f"{framesdir}/%05d.jpg"])
             nkfs = len(os.listdir(framesdir))
             if nkfs!= 1 + int((intdur - 1/self.fps -1) *self.fps):
                 print(f'wrong nkfs: {nkfs}, when intdur={intdur}, fps={self.fps} so predicted nkfs is {1 + int((intdur - 1/self.fps) *self.fps)}')
-            #timepoints = np.arange(1/self.fps, intdur, 1/self.fps)
             timepoints = np.linspace(1/self.fps, intdur, nkfs)
             np.save(join(framesdir, 'frametimes.npy'), timepoints)
-        elif os.path.isfile(join(framesdir, 'frametimes.npy')) and (not recompute):
-            timepoints = np.load(join(framesdir, 'frametimes.npy'))
         else:
             desired_nkfs = int(duration * self.fps)
             for fn in os.listdir(framesdir):
@@ -80,11 +83,64 @@ class SceneSegmenter():
                             os.remove(maybe_feat_fp)
                             print('removing', f'{self.base_framesdir}/{vidname}/{i:05d}.jpg','now n feat files is', len(os.listdir(f'{self.base_framesdir}/{vidname}')))
                 timepoints = timepoints[keep_idxs]
-            np.save(join(framesdir, 'frametimes.npy'), timepoints)
+            np.save(frames_fp, timepoints)
+            runtime = time()-starttime
+            os.makedirs('ss-runtimes/frames/', exist_ok=True)
+            with open(runtime_fp, 'w') as f:
+                f.write(str(runtime))
         if len(timepoints) != len(os.listdir(framesdir))-1:
             breakpoint()
         assert len(timepoints)+1 == len(os.listdir(framesdir)) # +1 for frametimes.npy
         return timepoints
+
+    def load_model(self):
+        if self.model_name == 'clip':
+            import open_clip
+            sys.stderr = io.StringIO()
+            self.model, self.preprocess = open_clip.create_model_from_pretrained('hf-hub:laion/CLIP-ViT-g-14-laion2B-s12B-b42K')
+            sys.stderr = sys.__stderr__
+            self.model = self.model.to(self.feats_device)
+            self.model.eval()
+            self.feat_fn = self.model.encode_image
+
+        elif self.model_name == 'blip':
+            from transformers import BlipProcessor, BlipForConditionalGeneration
+            sys.stderr = io.StringIO()
+            self.processor = BlipProcessor.from_pretrained('Salesforce/blip-image-captioning-large')
+            self.model = BlipForConditionalGeneration.from_pretrained('Salesforce/blip-image-captioning-large').to(self.feats_device)
+            sys.stderr = sys.__stderr__
+            self.model.eval()
+            self.feat_fn = lambda x: self.model.vision_model(pixel_values=x).last_hidden_state.mean(axis=1)
+            self.preprocess = lambda x: torch.clamp(self.processor(x, return_tensors='pt').pixel_values.to(self.feats_device).squeeze(0), 0, 1)
+
+        elif self.model_name == 'dinov2':
+        # DINOv2 version
+            import torchvision.transforms as transforms
+            sys.stderr = io.StringIO()
+            self.model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitg14')
+            sys.stderr = sys.__stderr__
+            self.model = self.model.to(self.feats_device)
+            self.model.eval()
+            self.preprocess = transforms.Compose([
+                transforms.Resize((224, 224)),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+            self.feat_fn = self.model
+
+        elif self.model_name == 'vit':
+        # ViT with timm version
+            import timm
+            sys.stderr = io.StringIO()
+            self.model = timm.create_model('vit_base_patch16_224', pretrained=True)
+            sys.stderr = sys.__stderr__
+            self.model = self.model.to(self.feats_device)
+            self.model.eval()
+            data_config = timm.data.resolve_model_data_config(self.model)
+            self.preprocess = timm.data.create_transform(**data_config, is_training=False)
+            self.feat_fn = self.model
+        else:
+            sys.exit(f'unrecognised model name: {self.model_name}')
 
     def batched_cost_under_params(self, x, mu, sigs):
         N, n, nz = x.shape
@@ -136,7 +192,8 @@ class SceneSegmenter():
         #assert (allclose(cost, neg_log_probs.sum(axis=0) - neg_log_probs.max() + self.prec_cost*nz*(n-1)))
         return cost
 
-    def segment_by_means(self, feats):
+    def segment_by_means(self, feats, vidname):
+        starttime = time()
         N = len(feats)
         max_seg_size = min(self.max_seg_size, N)
         x = np.unique(feats.flatten()).astype(float)
@@ -147,8 +204,9 @@ class SceneSegmenter():
         range_size = feat_vecs.max() - feat_vecs.min()
 
         direct_cost = torch.log(range_size) + self.prec_cost
-        batch_costs = torch.inf*torch.ones([N,N], device=self.device)
-        batch_costs.diagonal()[:] = feat_vecs.shape[1]*direct_cost
+        #batch_costs = torch.inf*torch.ones([N,N], device=self.device)
+        batch_costs = torch.inf*torch.ones([N,N])
+        batch_costs.diagonal()[:] = (feat_vecs.shape[1]*direct_cost).detach().cpu()
         bs = min(N, 20)
         if self.use_avg_sig:
             self.avg_sig = feat_vecs.unfold(0,bs,1).transpose(1,2).var(axis=1, keepdims=True).mean(axis=0)
@@ -179,12 +237,17 @@ class SceneSegmenter():
                 costs_to_set = torch.stack([costs[k:len(costs)-j+k] for k in range(j+1)]).mean(axis=0)
                 if not ( len(costs_to_set)==len(costs)-j):
                     breakpoint()
-                batch_costs.diagonal(offset=n+j-1)[:] = costs_to_set
+                batch_costs.diagonal(offset=n+j-1)[:] = costs_to_set.detach().cpu()
 
-        base_costs = batch_costs.detach().cpu().numpy()
+        #base_costs = batch_costs.detach().cpu().numpy()
+        base_costs = batch_costs.numpy()
         opt_splits, opt_cost = self.find_opt_cost(base_costs)
         if self.use_avg_sig:
             opt_cost += c_params_cost.item()
+
+        os.makedirs('ss-runtimes/find-opt-cost/', exist_ok=True)
+        with open(f'ss-runtimes/find-opt-cost/{vidname}', 'w') as f:
+            f.write(str(time()-starttime))
         return opt_splits
 
     def find_opt_cost(self, base_costs):
@@ -208,10 +271,12 @@ class SceneSegmenter():
         splits_fp = f'{self.cached_splits_dir}/{vidname}-inferred-vid-splits.npy'
         if os.path.exists(splits_fp) and not recompute:
             #print(f'loading splits from {splits_fp}')
-            self.kf_scene_split_points = np.load(splits_fp)
+            self.kf_scene_split_points = np.load(splits_fp).tolist()
         else:
             #print('computing span costs')
-            self.kf_scene_split_points = self.segment_by_means(feats_list)
+            with torch.no_grad():
+                self.kf_scene_split_points = self.segment_by_means(feats_list, vidname)
+            np.save(splits_fp, self.kf_scene_split_points)
         return self.kf_scene_split_points
 
     def scene_segment(self, vidname, recompute_keyframes, recompute_feats, recompute_best_split, bs, uniform_kfs):
@@ -222,19 +287,16 @@ class SceneSegmenter():
             breakpoint()
         sorted_fns = natsorted(fns)
 
-        os.makedirs(framefeatsdir:=f'{self.base_featsdir}/{vidname}', exist_ok=True)
+        os.makedirs(framefeatsdir:=os.path.join(self.base_featsdir, vidname), exist_ok=True)
 
         if recompute_feats or any(not os.path.exists(f'{framefeatsdir}/{x.split(".")[0]}.npy') for x in fns):
             if not hasattr(self, 'model'):
-                import open_clip
-                sys.stderr = io.StringIO()
-                self.model, self.preprocess = open_clip.create_model_from_pretrained('hf-hub:laion/CLIP-ViT-g-14-laion2B-s12B-b42K')
-                sys.stderr = sys.__stderr__
-                self.model = self.model.to(self.device)
-                self.model.eval()
+                self.load_model()
 
         feat_paths = [f'{framefeatsdir}/{x.split(".")[0]}.npy' for x in sorted_fns]
-        if recompute_feats or any(not os.path.exists(x) for x in feat_paths):
+        starttime = time()
+        runtime_fp=f'ss-runtimes/feats-extraction/{vidname}'
+        if recompute_feats or any(not os.path.exists(x) for x in feat_paths) or (not os.path.exists(runtime_fp)):
             feats_list = []
             batched = [(sorted_fns[i*bs:(i+1)*bs], feat_paths[i*bs:(i+1)*bs]) for i in range(int(math.ceil(len(sorted_fns)/bs)))]
             for i, (im_name_batch, featp_batch) in enumerate(pbar:=tqdm(batched)):
@@ -242,11 +304,12 @@ class SceneSegmenter():
                 im_list = []
                 for inb in im_name_batch:
                     image = Image.open(join(framesdir, inb))
-                    image = self.preprocess(image).unsqueeze(0).to(self.device)
+                    image = self.preprocess(image).unsqueeze(0).to(self.feats_device)
                     im_list.append(image)
                 image_batch = torch.cat(im_list)
                 with torch.no_grad():
-                    im_feats = self.model.encode_image(image_batch)
+                    #im_feats = self.model.encode_image(image_batch)
+                    im_feats = self.feat_fn(image_batch)
                     im_feats = im_feats.detach().cpu().numpy()
                 batch_fps = feat_paths[i*bs:(i+1)*bs]
                 for imf, ftp in zip(im_feats, batch_fps):
@@ -255,7 +318,13 @@ class SceneSegmenter():
         else:
             feats_list = [np.load(featp) for featp in feat_paths]
 
-        feats = np.stack(feats_list, axis=0)
+        try:
+            feats = np.stack(feats_list, axis=0)
+        except:
+            breakpoint()
+        os.makedirs('ss-runtimes/feats-extraction/', exist_ok=True)
+        with open(runtime_fp, 'w') as f:
+            f.write(str(time()-starttime))
         self.segment_from_feats_list(vidname, feats, recompute=recompute_best_split)
         pt = np.array([(timepoints[i]+timepoints[i+1])/2 for i in self.kf_scene_split_points])
         os.makedirs(kf_dir:=f'data/ffmpeg-keyframes-by-scene/{self.name_path}/{vidname}', exist_ok=True)
@@ -269,10 +338,10 @@ class SceneSegmenter():
                 next_scene_idx += 1
             if kf != 'frametimes.npy':
                 os.symlink(os.path.abspath(f'{framesdir}/{kf}'), os.path.abspath(f'{cur_scene_dir}/{kf}'))
+        print(len(pt))
         return pt, timepoints
 
 if __name__ == '__main__':
-
     import argparse
 
     parser = argparse.ArgumentParser()
@@ -282,9 +351,10 @@ if __name__ == '__main__':
     parser.add_argument('--recompute-all', action='store_true')
     #parser.add_argument('--data-dir', type=str, required=True)
     parser.add_argument('--dset', '-d', type=str, required=True)
-    parser.add_argument('--show-name', type=str, required=True)
-    parser.add_argument('--season', type=str, required=True)
-    parser.add_argument('--episode', type=str, required=True)
+    parser.add_argument('--show-name', type=str)
+    parser.add_argument('--model-name', type=str, choices=['clip', 'dinov2', 'vit', 'blip'], default='clip')
+    parser.add_argument('--season', type=str)
+    parser.add_argument('--episode', type=str)
     parser.add_argument('--cpu', action='store_true')
     parser.add_argument('--feats-bs', type=int, default=4, help='batch size for extracting features')
     parser.add_argument('--max-scene-len', type=int, default=240, help='maximum number of seconds that can be in a scene, lower is faster')
@@ -302,28 +372,29 @@ if __name__ == '__main__':
     device = 'cpu' if ARGS.cpu else 'cuda'
     if ARGS.dset in ['osvd', 'bbc', 'moviesumm']:
         assert ARGS.show_name is None
-        assert ARGS.season_name is None
+        assert ARGS.season is None
+        ARGS.episode = 'all'
     max_seg_size = int(ARGS.max_scene_len/ARGS.kf_every)
     print(max_seg_size)
-    if ARGS.episode == 'all':
-        def segment_season(seas):
-            print(f'splitting season {seas}')
-            ss = SceneSegmenter(ARGS.dset, ARGS.show_name, seas, max_seg_size, ARGS.pow_incr, ARGS.use_avg_sig, ARGS.kf_every, use_log_dist_cost=ARGS.use_log_dist_cost, device=device)
-            for fname in natsorted(os.listdir(ss.vid_dir)):
-                vidname = fname.removesuffix('.mp4')
-                #if vidname in ['episode_5', 'episode_18']:
-                #if vidname != 'episode_7':
-                    #continue
-                split_points, points_of_keyframes = ss.scene_segment(vidname, recompute_keyframes=ARGS.recompute_keyframes, recompute_feats=ARGS.recompute_frame_features, recompute_best_split=ARGS.recompute_best_split, bs=ARGS.feats_bs, uniform_kfs=ARGS.uniform_kfs)
-                print(vidname, '  '.join(f'{int(sp//60)}m{sp%60:.1f}s' for sp in split_points))
-        if ARGS.season == 'all':
-            seasons = sorted([x.removeprefix('season_') for x in os.listdir(f'data/full-videos/tvqa/{ARGS.show_name}')])
-            for s in seasons:
-                segment_season(s)
+    with torch.no_grad():
+        if ARGS.episode == 'all':
+            def segment_season(seas):
+                print(f'splitting season {seas}')
+                ss = SceneSegmenter(ARGS.dset, ARGS.show_name, seas, max_seg_size, ARGS.pow_incr, ARGS.use_avg_sig, ARGS.kf_every, use_log_dist_cost=ARGS.use_log_dist_cost, device=device, model_name=ARGS.model_name)
+                for fname in natsorted(os.listdir(ss.vid_dir)):
+                    vidname = fname.removesuffix('.mp4')
+                    #if vidname in ['episode_5', 'episode_18']:
+                    #if vidname != 'episode_7':
+                        #continue
+                    split_points, points_of_keyframes = ss.scene_segment(vidname, recompute_keyframes=ARGS.recompute_keyframes, recompute_feats=ARGS.recompute_frame_features, recompute_best_split=ARGS.recompute_best_split, bs=ARGS.feats_bs, uniform_kfs=ARGS.uniform_kfs)
+                    print(vidname, '  '.join(f'{int(sp//60)}m{sp%60:.1f}s' for sp in split_points))
+            if ARGS.season == 'all':
+                seasons = sorted([x.removeprefix('season_') for x in os.listdir(f'data/full-videos/tvqa/{ARGS.show_name}')])
+                for s in seasons:
+                    segment_season(s)
+            else:
+                segment_season(ARGS.season)
         else:
-            segment_season(ARGS.season)
-    else:
-        assert ARGS.season != 'all'
-        ss = SceneSegmenter(ARGS.dset, ARGS.show_name, ARGS.season, max_seg_size, ARGS.pow_incr, ARGS.use_avg_sig, ARGS.kf_every, use_log_dist_cost=ARGS.use_log_dist_cost, device=device)
-        split_points, points_of_keyframes = ss.scene_segment(f'episode_{ARGS.episode}', recompute_keyframes=ARGS.recompute_keyframes, recompute_feats=ARGS.recompute_frame_features, recompute_best_split=ARGS.recompute_best_split, bs=ARGS.feats_bs, uniform_kfs=ARGS.uniform_kfs)
-
+            assert ARGS.season != 'all'
+            ss = SceneSegmenter(ARGS.dset, ARGS.show_name, ARGS.season, max_seg_size, ARGS.pow_incr, ARGS.use_avg_sig, ARGS.kf_every, use_log_dist_cost=ARGS.use_log_dist_cost, device=device, model_name=ARGS.model_name)
+            split_points, points_of_keyframes = ss.scene_segment(f'episode_{ARGS.episode}', recompute_keyframes=ARGS.recompute_keyframes, recompute_feats=ARGS.recompute_frame_features, recompute_best_split=ARGS.recompute_best_split, bs=ARGS.feats_bs, uniform_kfs=ARGS.uniform_kfs)
